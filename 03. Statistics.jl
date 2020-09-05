1dc3a99fa99fd5637cfc2645a72dc909459cf741
jipq6175 <yc2253@cornell.edu>
2020-09-05 09:51:18 -0400

    03

--- 03. Statistics.jl
+++ 03. Statistics.jl
@@ -42,3 +42,44 @@ plot!(p.x,p.density .* length(myrandomvector) .*0.1, linewidth=3,color=2,label="
 
 
 # Probability distributions
+d = Normal()
+myrandomvector = rand(d,100000)
+histogram(myrandomvector)
+p=kde(myrandomvector)
+plot!(p.x,p.density .* length(myrandomvector) .*0.1, linewidth=3,color=2,label="kde fit") # nb of elements*bin width
+
+b = Binomial(40)
+myrandomvector = rand(b,1000000)
+histogram(myrandomvector)
+p=kde(myrandomvector)
+plot!(p.x,p.density .* length(myrandomvector) .*0.5,color=2,label="kde fit") # nb of elements*bin width
+
+
+# fit distributions
+x = rand(1000)
+d = fit(Normal, x)
+myrandomvector = rand(d,1000)
+histogram(myrandomvector,nbins=20,fillalpha=0.3,label="fit")
+histogram!(x,nbins=20,linecolor = :red,fillalpha=0.3,label="myvector")
+
+
+x = eruptions
+d = fit(Normal, x)
+myrandomvector = rand(d,1000)
+histogram(myrandomvector,nbins=20,fillalpha=0.3)
+histogram!(x,nbins=20,linecolor = :red,fillalpha=0.3)
+
+
+# T-Test
+ttest = OneSampleTTest(myrandomvector)
+
+
+# MLBase
+gt = [1,1,1,1,1,1,1,2];
+pred = [1,1,2,2,1,1,1,1];
+C = confusmat(2, gt, pred);
+C = C ./ sum(C, dims=2)
+correctrate(gt, pred)
+
+#
+scatter(eruptions, waittime)
